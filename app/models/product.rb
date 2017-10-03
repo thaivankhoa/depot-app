17e372b69b4db0fe288617f8795b10d5cc2ca998
thaivankhoa <thaivankhoa2802@gmail.com>
2017-10-03 17:00:42 +0700

    Smarter Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,16 @@ class Product < ApplicationRecord
              with:    %r{\.(gif|jpg|png)\Z}i,
              message: 'must be a URL for GIF, JPG or PNG image.'
              }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  has_many :line_items
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
